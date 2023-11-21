786007e248b220f9f8478dfcbf4a155a7a7e3aae
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2023-11-21 05:42:00 +0100

    Updated documentation & workflow (#25)

---
+++ appointment/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Adams Pierre David"
+__author_email__ = "adamspd.developer@gmail.com"
+__author_website__ = "https://adamspierredavid.com/"
+__description__ = "Managing appointment scheduling with customizable slots, staff features, and conflict handling."
+__package_name__ = "django-appointment"
+__url__ = "https://github.com/adamspd/django-appointment"
+__version__ = "2.1.1"
