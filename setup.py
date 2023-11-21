d744a765ba5aff41e4274f830d83f45724b079d8
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2023-11-21 22:58:50 +0100

    updated dependencies

--- setup.py
+++ setup.py
@@ -21,27 +21,42 @@ setuptools.setup(
     packages=setuptools.find_packages(where="appointments"),
     package_dir={"": "appointment"},
     include_package_data=True,
-    package_data={
-        'appointment': [
-            'static/css/**/*.css',
-            'static/js/**/*.js',
-            'static/img/**/*.*',
-            'templates/**/*.html',
-        ],
-        'appointment.tests': [
-            'mixins/*',
-            'base/*',
-            'models/*',
-            'utils/*',
-            '*.py'
-        ],
-    },
-    exclude_package_data={
-        "appointment": [
-            "django_appointment.egg-info"
-        ],
-        "appointment.django_appointment": [
-            "egg-info",
-        ]
-    },
+    classifiers=[
+        # Maturity of project
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Intended for
+        'Intended Audience :: Developers',
+
+        # License
+        "License :: OSI Approved :: Apache Software License",
+
+        # Python versions supported.
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+
+        # OS
+        "Operating System :: OS Independent",
+
+        # Topic
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+    license="Apache License 2.0",
+    python_requires='>=3.8',
+    install_requires=[
+        "Django>=4.2",
+        "Pillow>=10.1.0",
+        "phonenumbers>=8.13.22",
+        "django-phonenumber-field>=7.2.0",
+        "babel>=2.13.0",
+        "pytz>=2023.3",
+    ]
 )
