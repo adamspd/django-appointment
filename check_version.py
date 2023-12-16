6ce86f9a47e8ae4b9eb03b698918461a7f88a854
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2023-12-16 16:05:50 +0100

    Updated manifest, requirements and version handling process

--- check_version.py
+++ check_version.py
@@ -1,18 +1,24 @@
 import requests
-from appointment import __version__, __package_name__
+from appointment import __version__, __package_name__, __test_version__
 
-package_name = __package_name__
-current_version = __version__
 
-response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+def check_package_version(package_name, current_version):
+    version_exists = False
 
-version_exists = False
-
-if response.status_code == 200:
-    released_versions = response.json()["releases"].keys()
-    if current_version in released_versions:
-        print(f"Version {current_version} already exists on PyPI!")
+    if __test_version__:
+        print("Test version, skipping PyPI check.")
         version_exists = True
+    else:
+        response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
 
-# Output for GitHub Actions
-print(f"::set-output name=version_exists::{version_exists}")
+        if response.status_code == 200:
+            released_versions = response.json()["releases"].keys()
+            if current_version in released_versions:
+                print(f"Version {current_version} already exists on PyPI!")
+                version_exists = True
+
+    # Output for GitHub Actions
+    print(f"::set-output name=version_exists::{version_exists}")
+
+
+check_package_version(__package_name__, __version__)
