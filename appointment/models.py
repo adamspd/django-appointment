cec80610798a3a5c8b64e0757bd2d47f4fd7a064
Mathieu Dupuy <mathieu.dupuy@gitguardian.com>
2024-03-24 19:12:56 +0100

    Appointment: check start time is only strictly before end time

--- appointment/models.py
+++ appointment/models.py
@@ -310,7 +310,7 @@ class AppointmentRequest(models.Model):
 
     def clean(self):
         if self.start_time is not None and self.end_time is not None:
-            if self.start_time >= self.end_time:
+            if self.start_time > self.end_time:
                 raise ValidationError(_("Start time must be before end time"))
             if self.start_time == self.end_time:
                 raise ValidationError(_("Start time and end time cannot be the same"))
