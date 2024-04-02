124f7cabe0237950fe507116afb541b00e26334f
Johnathan Nader <john@nader.mx>
2024-04-02 15:56:58 -0600

    Update models.py
    Fix datetime issue with calandar

--- appointment/models.py
+++ appointment/models.py
@@ -31,13 +31,14 @@ PAYMENT_TYPES = (
 )
 
 DAYS_OF_WEEK = (
-    (0, 'Sunday'),
-    (1, 'Monday'),
-    (2, 'Tuesday'),
-    (3, 'Wednesday'),
-    (4, 'Thursday'),
-    (5, 'Friday'),
-    (6, 'Saturday'),
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
+
 )
 
 
