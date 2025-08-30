5b934f890e21b2a6d84fe9bff5ac71247586f82a
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2025-08-30 18:35:06 +0200

    new release and update django version

--- appointment/__init__.py
+++ appointment/__init__.py
@@ -6,5 +6,5 @@ __package_name__ = "django-appointment"
 __url__ = "https://github.com/adamspd/django-appointment"
 __package_website__ = "https://django-appt.adamspierredavid.com/"
 __package_doc_url__ = "https://django-appt-doc.adamspierredavid.com/"
-__version__ = "3.9.1b2"
-__test_version__ = True
+__version__ = "3.9.1"
+__test_version__ = False
