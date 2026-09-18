d269c8d8c548035239c11887a823caf667561968
Adams P. D. <57180807+adamspd@users.noreply.github.com>
2026-09-18 21:06:06 +0200

    Use plain gettext in views_admin like views

--- appointment/views_admin.py
+++ appointment/views_admin.py
@@ -13,7 +13,7 @@ import json
 from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404, redirect, render
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from django.views.decorators.http import require_POST
 
 from appointment.decorators import (
